7c598658b47e7c8364fa383d0cdc02ac5adb4ba9
384422148@qq.com <384422148@qq.com>
2017-12-25 22:35:41 +0800

    1225

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,240 +1,334 @@
 <template>
 	<div class="all">
-  <section class="first">
-  	 <div class="center">
-  	 <div class="box part1 lightcoral"><font class="logo p80">天</font></div>
-  	 <div class="box part2 lightcoral"><img src="../assets/img/part2.png"/></div>
-  	 <div class="box part3 lightcoral"><font class="logo p40">古琴</font></div>
-  	 <div class="box part4 lightcoral"><font class="logo p80">趣</font></div>
-  	 </div>
-  	 <div class="seal"><img src="../assets/img/seal.gif"/></div>
-  	 <div class="company">
-  	 <div><font>天趣古琴工作室</font></div>
-  	 <div><font>TIANQUGUQIN STUDIO</font></div>
-  	 </div>
-  </section>
- <section class="second">
-  	  <div class="firstpic">
-  	  	<img src="../assets/img/studio1.jpg" />
-  	  	<div class="desc"><p>
-  	     本方案是围绕现代简约为主题，适合于30岁左右的三口之家居住，再加上中式的设计元素在里面。
-		相互结合，相交融。以简洁明快的设计风格为主调，简洁和实用是现代简约风格的基本特点。简约风
-		格已经大行其道几年了，仍然保持很猛的势头，这是因为人们装修时总希望在经济、实用、舒适的同
-		时，体现一定的文化品味。而简约风格不仅注重居室的实用性，而且还体现出了现代社会生活的精致
-		与个性，符合现代人的生活品位。全面考虑，在总体布局上方面尽量满足业主生活的需求，主要装修
-		材料以樱桃木，壁纸为主，以樱桃木的优美含蓄，壁纸的朴素大方来装饰墙面的景点。更体现现代简
-		约的之感。创造一个温馨，健康的家庭环境。
-  	  	</p></div>
-  	  </div>
-  	  <div class="otherpic">
-  	  	<ul>
-  	  		<li>
-  	  			<div>
-  	  				<img src="../assets/img/studio1.jpg" />
-  	  			</div>
-  	  		</li>
-  	  		<li>
-  	  			<div>
-  	  				<img src="../assets/img/studio2.jpg" />
-  	  			</div>
-  	  		</li>
-  	  		<li>
-  	  			<div>
-  	  				<img src="../assets/img/studio4.jpg" />
-  	  			</div>
-  	  		</li>
-  	  		<li>
-  	  			<div>
-  	  				<img src="../assets/img/studio5.jpg" />
-  	  			</div>
-  	  		</li>
-  	  	</ul>
-  	  </div>
-  </section>
- 
- </div>
+		<section class="first">
+			<div class="center">
+				<div class="box part1 lightcoral">
+					<font class="logo p80">天</font>
+				</div>
+				<div class="box part2 lightcoral"><img src="../assets/img/background.jpg" /></div>
+				<div class="box part3 lightcoral">
+					<font class="logo p40">古琴</font>
+				</div>
+				<div class="box part4 lightcoral">
+					<font class="logo p80">趣</font>
+				</div>
+			</div>
+			<div class="seal"><img src="../assets/img/seal.gif" /></div>
+			<div class="company">
+				<div>
+					<font>天趣古琴工作室</font>
+				</div>
+				<div>
+					<font>TIANQUGUQIN STUDIO</font>
+				</div>
+			</div>
+		</section>
+		<section class="second">
+			<div class="firstpic">
+				<img src="../assets/img/studio1.jpg" />
+				<div class="desc">
+					<p>
+						本方案是围绕现代简约为主题，适合于30岁左右的三口之家居住，再加上中式的设计元素在里面。 相互结合，相交融。以简洁明快的设计风格为主调，简洁和实用是现代简约风格的基本特点。简约风 格已经大行其道几年了，仍然保持很猛的势头，这是因为人们装修时总希望在经济、实用、舒适的同 时，体现一定的文化品味。
+					</p>
+				</div>
+			</div>
+			<div class="otherpic">
+				<ul>
+					<li>
+						<div class="smallpic">
+							<img src="../assets/img/studio1.jpg" />
+						</div>
+					</li>
+					<li>
+						<div class="smallpic">
+							<img src="../assets/img/studio2.jpg" />
+						</div>
+					</li>
+					<li>
+						<div class="smallpic">
+							<img src="../assets/img/studio4.jpg" />
+						</div>
+					</li>
+					<li>
+						<div class="smallpic">
+							<img src="../assets/img/studio5.jpg" />
+						</div>
+					</li>
+				</ul>
+			</div>
+		</section>
+		<section class="third">
+			<div class="aboutme">
+				<div class="page">
+				<br />
+				<p>　　去年夏天，我们和S君夫妇在松堂住了三日。难得这三日的闲，我们约好了什么事不管，只玩儿，也带了两本书，却只是预备闲得真没办法时消消遣的。</p>
+				<p>　　出发的前夜，忽然雷雨大作。枕上颇为怅怅，难道天公这么不做美吗!第二天清早，一看却是个大晴天。上了车，一路树木带着宿雨，绿得发亮，地下只有一些水塘，没有一点尘土，行人也不多。又静，又干净。</p>
+				<p>　　想着到还早呢，过了红山头不远，车却停下了。两扇大红门紧闭着，门额是国立清华大学西山牧场。拍了一会门，没人出来，我们正在没奈何，一个过路的孩子说这门上了锁，得走旁门。旁门上接着牌子，“内有恶犬”。小时候最怕狗，有点趑趄。门里有人出来，保护着进去，一面吆喝着汪汪的群犬，一面只是说，“不碍不碍”。</p>
+				
+				
+				</div>
+			</div>
+			<div class="mypic">
+				<img src="../assets/img/500266185.jpg" />
+			</div>
+		</section>
+		<section class="fourth">
+			
+			</section>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld'
-}
+	export default {
+		name: 'HelloWorld'
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.all{
+	.all {
 		height: 100%;
-}
-.first{
-	  min-height: 720px;
-	  min-width: 1280px;
-	  height: 100%;
+	}
+	
+	.first {
+		min-height: 720px;
+		min-width: 1280px;
+		height: 100%;
 		position: relative;
 		color: #fff;
 		text-align: center;
 		margin: 0;
 		cursor: default;
-}
-.second{
+	}
 	
+	.second {
 		min-height: 720px;
-	  min-width: 1280px;
+		min-width: 1280px;
 		height: 100%;
-	  position: relative;
+		position: relative;
 		text-align: center;
 		margin: 0;
 		background: #3B3B3B;
-}
-
-.center{
-  width: 305px;  
-  height: 305px;  
-  overflow: auto;  
-  margin: auto;  
-  position: absolute;  
-  top: 0; left: 0; bottom: 0; right: 0;
-	background: white;
-	text-transform: uppercase;
-	letter-spacing: 3px;
-}
-.box{
-	width: 150px;
-	height: 150px;
-	float: left;
-	position:relative; 
-	border-radius: 15px;
-	text-align:center;
-	 line-height:150px;   
-}
-.part2{
-	right: -5px;
+	}
 	
-}
-.center img{
-	height: 100px;
-	width: 100px;
-	border:2px solid white;
-	border-radius: 50px;
-	overflow: auto;  
-  margin: auto;  
-  position: absolute;  
-  top: 0; left: 0; bottom: 5%; right: 0;
-}
-.part3{
-	bottom: -5px;
-}
-.part4{
-	right: -5px;
-	bottom: -5px;
-}
-
-.lightcoral {
-	background: #3B3B3B;
-}
-.logo{
-	font-family: myLogoFont;
+	.third {
+		min-height: 720px;
+		min-width: 1280px;
+		height: 100%;
+		position: relative;
+		text-align: center;
+		margin: 0;
+		background: #FFFFFF;
+	}
+	.fourth{
+		min-height: 720px;
+		min-width: 1280px;
+		height: 100%;
+		position: relative;
+		text-align: center;
+		margin: 0;
+		background: #FFFFFF;
+	}
+	.center {
+		width: 305px;
+		height: 305px;
+		overflow: auto;
+		margin: auto;
+		position: absolute;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		background: white;
+		text-transform: uppercase;
+		letter-spacing: 3px;
+	}
 	
-	color: white;
-}
-.p80{
-	font-size: 80px;
-}
-.p40{
-	font-size: 40px;
-}
-.seal{
-	width: 100px;  
-  height: 100px;  
-  overflow: auto;  
-  margin: auto;  
-  position: absolute;  
-  top: 430px; left: 260px; bottom: 0; right: 0;
-
-}
-.company{
-	width: 100%;  
-  height: 100px;  
-  overflow: auto;  
-  margin: auto;  
-  position: absolute;  
-  top: 450px; left: 0; bottom: 0; right: 0;
-  text-align: center;
-}
-.company font{
-	font-family: myLogoFont;
-	font-size: 20px;
-	color: #3B3B3B;
-}
-.seal img{
-	  height: 50px;
-	  width: 30px;
-}
-.desc{
-	width: 40%;
-	height: 40%;
-	opacity:0.6;  
-	background: black;
-	position: absolute;
-	top: 30%;
-	text-align: left;
-	overflow:hidden;
+	.box {
+		width: 150px;
+		height: 150px;
+		float: left;
+		position: relative;
+		border-radius: 15px;
+		text-align: center;
+		line-height: 150px;
+	}
 	
-}
-.desc p{
-	color:white ;
-	font-family: "微软雅黑";
-	font-size: 20px;
-	text-indent:2em; padding:0px; margin:0px;
-
-
-}
-.otherpic{
+	.part2 {
+		right: -5px;
+	}
 	
-	width: 20%;
-	height: 100%;
-	min-height: 504px;
-	min-width: 256px;
-	background: white;
-	float: right;
-}
-.otherpic ul {
-	 width: 100%;
-	 height: 100%;
-	 float:left; 
-	 list-style:none;
-	 margin: 0;
-	 padding: 0;
+	.center img {
+		height: 100px;
+		width: 100px;
+		border: 2px solid white;
+		border-radius: 50px;
+		overflow: auto;
+		margin: auto;
+		position: absolute;
+		top: 0;
+		left: 0;
+		bottom: 5%;
+		right: 0;
+	}
 	
-}
-.otherpic ul li {
-	 width: 100%;
-	 height: 25%;
-	 list-style-type:none;  
-}
-.otherpic ul li div {
-	 width: 100%;
-	 height: 100%;
-	 border: 8px solid #3B3B3B;
-}
-.otherpic ul li div img{
-	 width: 100%;
-	 height: 100%;
-	 
-}
-.firstpic{
-	position:absolute;
-	min-height: 540px;
-	min-width: 896px;
-	height:75%;
-	width: 70%;
-	background: white;
- 	top: 12.5%;
-}
-.firstpic img{
-	 height: 100%;
-	 width: 100%;
-	min-height: 504px;
-	min-width: 896px;
-}
+	.part3 {
+		bottom: -5px;
+	}
+	
+	.part4 {
+		right: -5px;
+		bottom: -5px;
+	}
+	
+	.lightcoral {
+		background: #3B3B3B;
+	}
+	
+	.logo {
+		font-family: myLogoFont;
+		color: white;
+	}
+	
+	.p80 {
+		font-size: 80px;
+	}
+	
+	.p40 {
+		font-size: 40px;
+	}
+	
+	.seal {
+		width: 100px;
+		height: 100px;
+		overflow: auto;
+		margin: auto;
+		position: absolute;
+		top: 430px;
+		left: 260px;
+		bottom: 0;
+		right: 0;
+	}
+	
+	.company {
+		width: 100%;
+		height: 100px;
+		overflow: auto;
+		margin: auto;
+		position: absolute;
+		top: 450px;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		text-align: center;
+	}
+	
+	.company font {
+		font-family: myLogoFont;
+		font-size: 20px;
+		color: #3B3B3B;
+	}
+	
+	.seal img {
+		height: 50px;
+		width: 30px;
+	}
+	
+	.desc {
+		width: 40%;
+		height: 40%;
+		opacity: 0.6;
+		background: black;
+		position: absolute;
+		top: 30%;
+		text-align: left;
+		overflow: hidden;
+	}
+	
+	.desc p {
+		color: white;
+		font-family: "微软雅黑";
+		font-size: 20px;
+		text-indent: 2em;
+		padding: 0px;
+		margin: 0px;
+	}
+	
+	.otherpic {
+		width: 20%;
+		height: 100%;
+		min-height: 504px;
+		min-width: 256px;
+		background: white;
+		float: right;
+	}
+	
+	.otherpic ul {
+		width: 100%;
+		height: 100%;
+		float: left;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+	
+	.otherpic ul li {
+		width: 100%;
+		height: 25%;
+		background: #3B3B3B;
+		/*border: 4px solid #3B3B3B;*/
+	}
+	
+	.smallpic {
+		width: 100%;
+		height: 100%;
+	}
+	
+	.smallpic img {
+		width: 100%;
+		height: 95%;
+	}
+	
+	.firstpic {
+		position: absolute;
+		min-height: 540px;
+		min-width: 896px;
+		height: 75%;
+		width: 70%;
+		background: white;
+		top: 12.5%;
+	}
+	
+	.firstpic img {
+		height: 100%;
+		width: 100%;
+		min-height: 504px;
+		min-width: 896px;
+	}
+	
+	.aboutme {
+		width: 60%;
+		height: 100%;
+		float: left;
+		background: white;
+		color: #3B3B3B;
+	}
+	.page{
+		width: 50%;
+		height: 100%;
+		margin: 0 auto;
+		text-align: left;
+
+	}
+	.mypic {
+		width: 40%;
+		height: 100%;
+		float: left;
+		background: white;
+	}
+	
+	.mypic img {
+		width: 100%;
+		height: 100%;
+	}
 </style>
--- src/components/HelloWorld2.vue
+++ src/components/HelloWorld2.vue
@@ -1,51 +1,98 @@
 <template>
-  <div class="hello">
-  	<img src="../assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div class="all">
+		<section class="first">
+			<transition name="fold">
+				<div class="center">
+					<img src="../assets/img/background.jpg" />
+				</div>
+				<div class="company">
+  	 <div><font>天趣古琴工作室</font></div>
+  	 <div><font>TIANQUGUQIN STUDIO</font></div>
+  	 </div>
+			</transition>
+		</section>
+
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld2',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App2'
-    }
-  }
-}
+	export default {
+		name: 'HelloWorld2'
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+	.all {
+		height: 100%;
+	}
+	
+	.first {
+		min-height: 720px;
+		min-width: 1280px;
+		height: 100%;
+		position: relative;
+		color: #fff;
+		text-align: center;
+		margin: 0;
+		cursor: default;
+	}
+	
+	.second {
+		min-height: 720px;
+		min-width: 1280px;
+		height: 100%;
+		position: relative;
+		text-align: center;
+		margin: 0;
+		background: #3B3B3B;
+	}
+	
+	.center {
+		width: 355px;
+		height: 305px;
+		overflow: auto;
+		margin: auto;
+		position: absolute;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		background: white;
+		text-transform: uppercase;
+		letter-spacing: 3px;
+	}
+	
+	.center img {
+		width: 350px;
+		height: 300px;
+		-webkit-filter: blur(0.5px);
+		/* Chrome, Opera */
+		-moz-filter: blur(0.5px);
+		-ms-filter: blur(0.5px);
+		filter: blur(0.5px);
+	}
+	.company{
+	width: 100%;  
+  height: 100px;  
+  overflow: auto;  
+  margin: auto;  
+  position: absolute;  
+  top: 450px; left: 0; bottom: 0; right: 0;
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.company font{
+	font-family: myLogoFont;
+	font-size: 20px;
+	color: #3B3B3B;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+	.fold-enter-active,
+	.fold-leave-active {
+		transition: all 10.5s;
+	}
+	
+	.fold-enter,
+	.fold-leave-active {
+		transform: translate3d(0, 0, 0);
+	}
 </style>
